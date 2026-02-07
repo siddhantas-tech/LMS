1b4af5a86d4f24786eefb7c49412e6cce49cf0bc
siddhantas-tech <siddhant.as@somaiya.edu>
2026-02-07 22:46:10 +0530

    Remove unused private routes files

--- frontend/src/routes/PrivateRoute.tsx
+++
@@ -1,9 +0,0 @@
-import { Navigate, Outlet } from 'react-router-dom';
-
-const PrivateRoute = () => {
-    const isAuthenticated = !!localStorage.getItem('token');
-
-    return isAuthenticated ? <Outlet /> : <Navigate to="/login" replace />;
-};
-
-export default PrivateRoute;
