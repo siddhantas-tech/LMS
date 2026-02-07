7e05fd7b538fe9a1431a67228bc1c8f303d07457
siddhantas-tech <siddhant.as@somaiya.edu>
2026-02-07 16:27:37 +0530

    routing issue

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import CoursesPage from './pages/Courses';
 import CoursePlayer from './pages/CoursePlayer';
 import DashboardPage from './pages/Dashboard';
